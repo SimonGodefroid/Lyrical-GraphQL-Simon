2a8abd88724322f64438414f9bd5110b7ae9b815
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-06 12:35:00 +0100

    wired the component with graphql and management to pass props to gql query

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
+	componentDidMount() {
+		// this.props.query({});
+	}
 	render() {
+		console.log(this.props);
 		return (
 			<div>
 				<h3>Song Detail</h3>
@@ -9,4 +15,10 @@ class SongDetail extends Component {
 		);
 	}
 }
-export default SongDetail;
+export default graphql(fetchSong, {
+	options: props => {
+		return {
+			variables: { id: props.params.id }
+		};
+	}
+})(SongDetail);
