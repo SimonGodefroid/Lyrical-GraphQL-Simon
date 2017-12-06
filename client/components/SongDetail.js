7cf0569c8fd6bc80331ac499fda31c407c665518
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-06 14:30:05 +0100

    added mutation to add lyrics

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import fetchSong from '../queries/fetchSong';
 import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
 class SongDetail extends Component {
 	render() {
 		const { song } = this.props.data;
@@ -12,6 +13,7 @@ class SongDetail extends Component {
 				<div>
 					<Link to={`/`}>Back</Link>
 					<h3>{song.title}</h3>
+					<LyricCreate songId={this.props.params.id} />
 				</div>
 			);
 		}
