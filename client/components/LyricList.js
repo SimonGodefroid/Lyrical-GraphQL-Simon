8ba3ed6564f7ab8d0af587f14757e485c1ef3c97
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-06 15:17:56 +0100

    added likes mutation

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 class LyricList extends Component {
 	onLike(id) {
-		console.log(id);
+		this.props.mutate({ variables: { id } });
 	}
 	renderLyrics() {
-		return this.props.lyrics.map(({ id, content }) => {
+		return this.props.lyrics.map(({ id, content, likes }) => {
 			return (
 				<li className={`collection-item`} key={id}>
 					{content}
 					<i className={`material-icons`} onClick={() => this.onLike(id)}>
 						thumb_up
 					</i>
+					{likes}
 				</li>
 			);
 		});
@@ -21,4 +23,13 @@ class LyricList extends Component {
 	}
 }
 
-export default LyricList;
+const mutation = gql`
+	mutation LikeLyric($id: ID!) {
+		likeLyric(id: $id) {
+			id
+			likes
+		}
+	}
+`;
+
+export default graphql(mutation)(LyricList);
