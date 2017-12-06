2dae331dad543995e1299d97b07368b94270bf6d
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-06 15:33:28 +0100

    fixed bug in mutation by fetching likes

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -34,6 +34,7 @@ const mutation = gql`
 			lyrics {
 				id
 				content
+				likes
 			}
 		}
 	}
--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 class LyricList extends Component {
-	onLike(id) {
-		this.props.mutate({ variables: { id } });
+	onLike(id, likes) {
+		this.props.mutate({
+			variables: { id },
+			optimisticResponse: {
+				__typename: 'Mutation',
+				likeLyric: {
+					id,
+					__typename: 'LyricType',
+					likes: likes + 1
+				}
+			}
+		});
 	}
 	renderLyrics() {
 		return this.props.lyrics.map(({ id, content, likes }) => {
 			return (
 				<li className={`collection-item`} key={id}>
 					{content}
-					<i className={`material-icons`} onClick={() => this.onLike(id)}>
-						thumb_up
-					</i>
-					{likes}
+					<div className={`vote-box`}>
+						<i className={`material-icons`} onClick={() => this.onLike(id, likes)}>
+							thumb_up
+						</i>
+						{likes}
+					</div>
 				</li>
 			);
 		});
