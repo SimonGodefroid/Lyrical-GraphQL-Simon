c62b9d8c7309d4034efc143e4eae03f941c4947b
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-06 11:43:46 +0100

    added mutation handling with refetch and refactored query

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 class SongCreate extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { title: '' };
-		// this.onHandleChange = this.onHandleChange.bind(this);
 	}
-	// onHandleChange(evt) {
-	// 	this.setState({
-	// 		title: evt.target.value
-	// 	});
-	// }
 
 	onSubmit(event) {
 		event.preventDefault();
+		this.props
+			.mutate({
+				variables: { title: this.state.title },
+				refetchQueries: [{ query }]
+			})
+			.then(() => hashHistory.push('/'));
 	}
+
 	render() {
 		return (
 			<div className={``}>
+				<Link to={`/`}>Back</Link>
 				<h3>Create a new song</h3>
 				<form className={``} onSubmit={this.onSubmit.bind(this)}>
 					<label>Song Title:</label>
@@ -33,12 +38,11 @@ class SongCreate extends Component {
 	}
 }
 const mutation = gql`
-	mutation {
-		addSong(title: "Dog call") {
-			id
+	mutation AddSong($title: String) {
+		addSong(title: $title) {
 			title
 		}
 	}
 `;
 
-export default SongCreate;
+export default graphql(mutation)(SongCreate);
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 class SongList extends Component {
 	renderSongs() {
 		return this.props.data.songs.map(song => {
@@ -17,21 +19,14 @@ class SongList extends Component {
 		} else {
 			return (
 				<div>
-					List of Songs
+					List of Songs:
 					<ul className={`collection`}>{this.renderSongs()}</ul>
+					<Link to={`/songs/new`} className={`btn-floating btn-large red right`}>
+						<i className={`material-icons`}>add</i>
+					</Link>
 				</div>
 			);
 		}
 	}
 }
-
-const query = gql`
-	{
-		songs {
-			id
-			title
-		}
-	}
-`;
-
 export default graphql(query)(SongList);
