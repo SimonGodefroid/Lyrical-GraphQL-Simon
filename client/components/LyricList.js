6014776101b9d2dcbe34354aa71f1ff6d8ee1856
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-06 15:08:32 +0100

    added the thumbs up and like triggering

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+	onLike(id) {
+		console.log(id);
+	}
 	renderLyrics() {
 		return this.props.lyrics.map(({ id, content }) => {
 			return (
 				<li className={`collection-item`} key={id}>
 					{content}
+					<i className={`material-icons`} onClick={() => this.onLike(id)}>
+						thumb_up
+					</i>
 				</li>
 			);
 		});
